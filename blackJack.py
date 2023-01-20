b96ef10b020e2e84f84ada2aa03c60d00e859adf
Esra404 <esradrmz571@gmail.com>
2023-01-21 02:27:22 +0300

    berdan

---
+++ blackJack.py
@@ -0,0 +1,8 @@
+import random
+#deste      oyuncu1 oyuncu2 
+deste=[2,3,4,5,6,7,8,9,10,'J','Q','K','A']*4
+oyuncuBir=[]
+oyuncuİki=[]
+def kartHesapla(donus):
+    donus=random.choice(deste)
+    deste=
